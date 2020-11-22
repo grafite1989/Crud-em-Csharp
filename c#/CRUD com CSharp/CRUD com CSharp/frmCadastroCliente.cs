32f2b0d3ee91c1d6e7106b2f54a833fa423279cf
André Luis Costa <35852516+grafite1989@users.noreply.github.com>
2020-11-22 10:28:55 -0300

    executando listagem 3

--- c#/CRUD com CSharp/CRUD com CSharp/frmCadastroCliente.cs
+++ c#/CRUD com CSharp/CRUD com CSharp/frmCadastroCliente.cs
@@ -21,7 +21,19 @@ namespace CRUD_com_CSharp
 
         private void frmCadastroCliente_Load(object sender, EventArgs e)
         {
-
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tstId.Enabled = true;
+            tsbBuscar.Enabled = true;
+            txtNome.Enabled = false;
+            txtEndereco.Enabled = false;
+            mskCEP.Enabled = false;
+            txtBairro.Enabled = false;
+            txtCidade.Enabled = false;
+            txtUf.Enabled = false;
+            mskTelefone.Enabled = false;
         }
     }
 }
